c26cdafeb54dbd615657af1dde1d4b6cd071b383
naomiuwa <naomi.uwamungu@gmail.com>
2023-10-03 16:42:37 +0100

    turned one if statement into a function which takes player1 and player2 as parameters

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,34 +1,61 @@
-let playerMove = "scissors"; 
-let computerMove = "paper"; 
+let playerMove = "scissors";
+let computerMove = "scissors";
 
-//write if statements that will determine the winner of the game 
+//TASK 1
+//write if statements that will determine the winner of the game
 //e.g if playerMove chooses 'rock' & computerMove chooses 'paper', computerMove wins!
 //change the hard-coded values for playerMove and computerMove
-//console.log the expected outcome (win, lose, draw) in each if statement 
+//console.log the expected outcome (win, lose, draw) in each if statement
 
-//if playerMove chooses 'rock' & computerMove chooses 'paper' - computerMove wins 
-//if playerMove chooses 'paper' & computerMove chooses 'rock' - playerMove wins 
+//if playerMove chooses 'rock' & computerMove chooses 'paper' - computerMove wins
+//if playerMove chooses 'paper' & computerMove chooses 'rock' - playerMove wins
 
 if (playerMove === "rock" && computerMove === "paper") {
-    //console.log("Computer Wins!")
+  //console.log("Computer Wins!")
+} else if (playerMove === "paper" && computerMove === "rock") {
+  //console.log("Player Wins!")
 } else {
-    //console.log("Player Wins!")
-}; 
+  //console.log("It's a draw!");
+}
 
-//if playerMove chooses 'rock' & computerMove chooses 'scissors' - playerMove wins 
+//if playerMove chooses 'rock' & computerMove chooses 'scissors' - playerMove wins
 //if playerMove chooses 'scissors' & computerMoves chooses 'rock' - computerMove wins
 
 if (playerMove === "rock" && computerMove === "scissors") {
-    //console.log("Player Wins!")
+  //console.log("Player Wins!")
+} else if (playerMove === "scissors" && computerMove === "rock") {
+  //console.log("Computer Wins!")
 } else {
-    //console.log("Computer Wins!")
+  //console.log("It's a draw!");
 }
 
-//if playerMove chooses 'paper' & computerMove chooses 'scissors' - computerMove wins 
-//if playerMove chooses 'scissors' & computerMove chooses 'paper' - playerMove wins 
+//if playerMove chooses 'paper' & computerMove chooses 'scissors' - computerMove wins
+//if playerMove chooses 'scissors' & computerMove chooses 'paper' - playerMove wins
 
 if (playerMove === "paper" && computerMove === "scissors") {
-    console.log("Computer Wins!") 
+  //console.log("Computer Wins!");
+} else if (playerMove === "scissors" && computerMove === "paper") {
+  //console.log("Player Wins!");
 } else {
-    console.log("Player Wins!")
+  //console.log("It's a draw!");
 }
+
+//TASK 2
+//turn the if statements into a function
+//set up a function which takes in player1 and player2 as parameters
+//if player1 wins return 1
+//if player2 wins return -1
+//if its a draw return 0
+
+function getWinner (player1, player2) {
+    if (player1 === "paper" && player2 === "rock") {
+        return 1; 
+    } else if (player1 === "rock" && player2 === "paper") {
+        return -1; 
+    } else {
+        return 0; 
+    }
+}
+
+let result = getWinner("paper", "paper"); 
+console.log(result)
