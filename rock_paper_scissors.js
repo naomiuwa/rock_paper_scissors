cabecffaaf1ccaafb226661699251358fc9da8f7
naomiuwa <naomi.uwamungu@gmail.com>
2023-10-04 10:18:46 +0100

    used the prompt method to get user's input value. Called the function with the user's value and hard-coded the computer's move

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -47,15 +47,39 @@ if (playerMove === "paper" && computerMove === "scissors") {
 //if player2 wins return -1
 //if its a draw return 0
 
-function getWinner (player1, player2) {
-    if (player1 === "paper" && player2 === "rock" || player1 === "rock" && player2 === "scissors" || player1 === "scissors" && player2 === "paper") {
-        return 1; 
-    } else if (player1 === "rock" && player2 === "paper" || player1 === "scissors" && player2 === "rock" || player1 === "paper" && player2 === "scissors") {
-        return -1; 
-    } else {
-        return 0; 
-    }
+//TASK 3
+//use the prompt method to get player1 to enter their choice or value
+//call the function with player1's value 
+//hard-code player2's (computer's) value 
+//use the alert method to display the correct result e.g 1, -1 or 0
+
+//the prompt method gives you back the value entered by the user or player1
+const player1Choice = prompt("Choose your tool"); 
+console.log(player1Choice); 
+
+function getWinner(player1, player2) {
+  /* Example for first if statement: if player1 chooses ‘paper’ & player2 chooses ‘rock’ or player1 chooses ‘rock’ & player2 chooses ‘scissors’ or player1 chooses ‘scissors’ & player2 chooses ‘paper’*/
+
+  if (
+    (player1 === "paper" && player2 === "rock") ||
+    (player1 === "rock" && player2 === "scissors") ||
+    (player1 === "scissors" && player2 === "paper")
+  ) {
+    return 1;
+  } else if (
+    (player1 === "rock" && player2 === "paper") ||
+    (player1 === "scissors" && player2 === "rock") ||
+    (player1 === "paper" && player2 === "scissors")
+  ) {
+    return -1;
+  } else {
+    return 0;
+  }
 }
 
-let result = getWinner("scissors", "scissors"); 
+let result = getWinner(player1Choice, "rock");
 console.log(result)
+
+
+
+
