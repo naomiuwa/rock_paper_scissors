06b7afac7f381b0ef2131b67cc2023573ec13823
naomiuwa <naomi.uwamungu@gmail.com>
2023-10-04 14:10:08 +0100

    wrote a function that generates a random choice/ move for the computer

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -49,13 +49,39 @@ if (playerMove === "paper" && computerMove === "scissors") {
 
 //TASK 3
 //use the prompt method to get player1 to enter their choice or value
-//call the function with player1's value 
-//hard-code player2's (computer's) value 
+//call the function with player1's value
+//hard-code player2's (computer's) value
 //use the alert method to display the correct result e.g 1, -1 or 0
 
 //the prompt method gives you back the value entered by the user or player1
-const player1Choice = prompt("Choose your tool"); 
-console.log(player1Choice); 
+const player1Choice = prompt("Choose your tool");
+//console.log(player1Choice);
+
+//TASK 4
+//write a function that generates a choice/ move for the computer
+//user enters choice in the prompt
+//computer makes choice
+//the correct score shows in the alert
+
+//PLAN
+//set up a function with no parameters
+//define a variable to store the choices in an array
+//use Math.random() to generate a random choice from the length of choices and store it in a variable
+//define a variable to access the element at the randomly generated index in the array, returning one of the choices: "rock", "paper", or "scissors".
+//return the variable
+//call the function
+//change the "player2" parameter to the returned variable in the getWinner function being called
+
+function player2Choice() {
+    const choices = ["rock", "paper", "scissors"]; 
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  const computerMove = choices[randomIndex]; 
+  return computerMove; 
+
+}
+
+const computerChoice = player2Choice();
+console.log(computerChoice); 
 
 function getWinner(player1, player2) {
   /* Example for first if statement: if player1 chooses ‘paper’ & player2 chooses ‘rock’ or player1 chooses ‘rock’ & player2 chooses ‘scissors’ or player1 chooses ‘scissors’ & player2 chooses ‘paper’*/
@@ -77,9 +103,4 @@ function getWinner(player1, player2) {
   }
 }
 
-let result = getWinner(player1Choice, "paper");
-
-
-
-
-
+let result = getWinner(player1Choice, computerChoice);
---
+++ rock_paper_scissors.test.js
@@ -0,0 +1,2 @@
+//TESTS
+//the result alert should not appear without the user entering a value 
